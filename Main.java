1fa7ac20150529919e9f63f5bbf8c5a37c808531
Nhat Tram Nguyen <145398919+tramnngy@users.noreply.github.com>
2025-10-17 21:46:48 +0000

    finished problems 1 & 2

--- Main.java
+++ Main.java
@@ -4,6 +4,43 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    // Problem 1 
+    Scanner number = new Scanner (System.in);
+   
+    int N = 0; 
+    int sum = 0; 
+    while (N != -1)
+    {
+       System.out.print("Type a non-negative number:");
+       N =  number.nextInt();
+       sum += N ; 
+    }
+    sum++; 
+    System.out.println ( "The sum is: " + sum ); 
+    
+
+
+    // Problem 2
+    Scanner spots = new Scanner (System.in);
+    System.out.println("How many numbers do you want to input? ");
+    int amount = spots.nextInt();
+    int count = 0 ; 
+    int max = 0 ; 
+    int userInput = 0; 
+    while ( count <= amount)
+    {
+      Scanner input = new Scanner (System.in);
+      System.out.println("Type in your number:");
+      userInput = input.nextInt(); 
+
+       if ( userInput > max)
+       {
+        max = userInput;
+       }
+       count++; 
+    }
+    System.out.println("The largest score is: " + max );
+
+
   }
 }
